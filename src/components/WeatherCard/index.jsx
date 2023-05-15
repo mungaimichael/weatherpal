58aca2fc5f25a733d22ce272e021b23fd013e8c1
mungaimichael <mungaimikey@gmail.com>
2023-05-15 12:43:40 +0300

    added the different views available

--- src/components/WeatherCard/index.jsx
+++ src/components/WeatherCard/index.jsx
@@ -1,28 +1,55 @@
-import { useState } from "react"
-import styles from "../../styles/weatherCard.module.css"
+import { useState } from "react";
+import "../../styles/main.css"
 
 
 const index = () => {
-    const [query, setQuery] = useState(' ');
-    
+
+    const [query, setQuery] = useState('');
     return (
-        <div className={styles.left_card}>
-            <form
-                className={styles.search_input}
-                onSubmit={(e) => { e.preventDefault(); console.log(query) }}
-            >
-                <input
-                    type="text"
-                    name="query"
-                    placeholder="where is your location?"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                />
-                <a type="submit" >
-                    <img src="/media/search.png" alt="search icon" />
-                </a>
-            </form>
-        </div>
+        <>
+
+            <div className="weather-card" >
+                <form
+                >
+                    <div className="inputs">
+                        <input
+                            type="text"
+                            name="query"
+                            placeholder="where is your location?"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <a type="submit"
+                            onClick={(e) => {
+                                e.preventDefault();
+                            }}
+
+                        >
+                            <img src="/media/search.png" alt="search icon" />
+                        </a>
+
+                    </div>
+                </form>
+
+                <div className="weather">
+                    <img src="/media/5.png" alt="weather icon" />
+                    <span id="temp" >
+                        22C
+                    </span>
+                    <span>
+                        Monday, May 8th
+                    </span>
+                    <span>
+                        Mostly Cloudy
+                    </span>
+                    <span>
+                        Limuru
+                    </span>
+                </div>
+            </div>
+
+        </>
+
     )
 }
 
